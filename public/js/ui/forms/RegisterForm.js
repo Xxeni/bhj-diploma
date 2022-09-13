2e3733a7887134b562eea234c02a5903c4e642e1
Ксения <ksenia2833@gmail.com>
2022-09-13 12:54:05 +0400

    Update RegisterForm.js

--- public/js/ui/forms/RegisterForm.js
+++ public/js/ui/forms/RegisterForm.js
@@ -2,7 +2,7 @@
  * Класс RegisterForm управляет формой
  * регистрации
  * */
-class RegisterForm extends AsyncForm {
+ class RegisterForm extends AsyncForm {
   /**
    * Производит регистрацию с помощью User.register
    * После успешной регистрации устанавливает
@@ -10,6 +10,18 @@ class RegisterForm extends AsyncForm {
    * и закрывает окно, в котором находится форма
    * */
   onSubmit(data) {
-
+  	User.register(data, callback);
+  	const formControl = Array.prototype.slice.call(document.querySelectorAll('form[id="register-form"] > .form-group > .form-control'));
+  	const modalRegister = document.getElementById('modal-register');
+  	function callback(err, response) {
+  		if (response.success) {
+  			for (let i = 0; i < formControl.length; i++) {
+  				formControl[i].value = '';
+  			}
+  			App.setState('user-logged');
+  			const modalForm = new Modal(modalRegister);
+  			modalForm.close();
+  		}
+  	}
   }
 }
