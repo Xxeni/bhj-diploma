02d82631376e511bfae353fca34e6ca01875b285
Ксения <ksenia2833@gmail.com>
2022-09-13 12:53:21 +0400

    Update CreateTransactionForm.js

--- public/js/ui/forms/CreateTransactionForm.js
+++ public/js/ui/forms/CreateTransactionForm.js
@@ -2,13 +2,14 @@
  * Класс CreateTransactionForm управляет формой
  * создания новой транзакции
  * */
-class CreateTransactionForm extends AsyncForm {
+ class CreateTransactionForm extends AsyncForm {
   /**
    * Вызывает родительский конструктор и
    * метод renderAccountsList
    * */
   constructor(element) {
     super(element)
+    this.renderAccountsList();
   }
 
   /**
@@ -16,7 +17,21 @@ class CreateTransactionForm extends AsyncForm {
    * Обновляет в форме всплывающего окна выпадающий список
    * */
   renderAccountsList() {
-
+    Account.list(null, callback);
+    const accountListIncome = this.element.querySelector('#income-accounts-list');
+    const accountListExpense = this.element.querySelector('#expense-accounts-list');
+    let accounts = '';
+    function callback(err, response) {
+      for (let a in response.data) {
+        if (accountListIncome) {
+          accounts += `<option value="`+ response.data[a].id +`">`+ response.data[a].name +`</option>`;
+          accountListIncome.innerHTML = accounts;
+        } else if (accountListExpense) {
+          accounts += `<option value="`+ response.data[a].id +`">`+ response.data[a].name +`</option>`;
+          accountListExpense.innerHTML = accounts;
+        }        
+      }      
+    }
   }
 
   /**
@@ -26,6 +41,24 @@ class CreateTransactionForm extends AsyncForm {
    * в котором находится форма
    * */
   onSubmit(data) {
-
+    Transaction.create(data, callback);
+    let formControl = this.element.querySelectorAll('.form-control');
+    const newIncome = document.getElementById('modal-new-income');
+    const newExpense = document.getElementById('modal-new-expense');
+    function callback(err, response) {
+      if (response) {
+        for (let i = 0; i < formControl.length; i++) {
+          formControl[i].value = '';
+        }
+        if (data.type === 'income') {
+          const modalForm = new Modal(newIncome);
+          modalForm.close();
+        } else if (data.type === 'expense') {
+          const modalForm = new Modal(newExpense);
+          modalForm.close();
+        }        
+        App.update();
+      } 
+    }
   }
 }
