645d8a17ba8f44da9126b30437a7f5df70651aa9
Ксения <ksenia2833@gmail.com>
2022-09-13 12:52:41 +0400

    Update CreateAccountForm.js

--- public/js/ui/forms/CreateAccountForm.js
+++ public/js/ui/forms/CreateAccountForm.js
@@ -2,13 +2,23 @@
  * Класс CreateAccountForm управляет формой
  * создания нового счёта
  * */
-class CreateAccountForm extends AsyncForm {
+ class CreateAccountForm extends AsyncForm {
   /**
    * Создаёт счёт с помощью Account.create и закрывает
    * окно в случае успеха, а также вызывает App.update()
    * и сбрасывает форму
    * */
   onSubmit(data) {
-
+  	Account.create(data, callback);
+  	const formControl = document.querySelectorAll('form[id="new-account-form"] > .form-group > .form-control')[0];
+  	const newAccountModal = document.getElementById('modal-new-account');
+  	function callback(err, response) {
+  		if (response) {
+  			formControl.value = '';
+  			const modalForm = new Modal(newAccountModal);
+  			modalForm.close();
+  			App.update();
+  		}  	
+  	}
   }
 }
