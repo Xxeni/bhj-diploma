59373ee7aaac3dc2ff6bd7381415bcbbaa2961fd
Ксения <ksenia2833@gmail.com>
2022-09-13 12:57:10 +0400

    Update TransactionsWidget.js

--- public/js/ui/widgets/TransactionsWidget.js
+++ public/js/ui/widgets/TransactionsWidget.js
@@ -4,7 +4,7 @@
  * создания нового дохода или расхода
  * */
 
-class TransactionsWidget {
+ class TransactionsWidget {
   /**
    * Устанавливает полученный элемент
    * в свойство element.
@@ -12,7 +12,12 @@ class TransactionsWidget {
    * необходимо выкинуть ошибку.
    * */
   constructor( element ) {
-
+    if (element) {
+      this.element = element;
+      this.registerEvents();
+    } else {
+      throw new Error('Элемент не существует');
+    } 
   }
   /**
    * Регистрирует обработчики нажатия на
@@ -21,6 +26,13 @@ class TransactionsWidget {
    * экземпляра окна
    * */
   registerEvents() {
-
+    const createIncome = this.element.querySelector('.create-income-button');
+    const createExpense = this.element.querySelector('.create-expense-button');
+    createIncome.onclick = function() {
+      App.getModal('newIncome').open();
+    }
+    createExpense.onclick = function() {
+      App.getModal('newExpense').open();
+    }
   }
 }
