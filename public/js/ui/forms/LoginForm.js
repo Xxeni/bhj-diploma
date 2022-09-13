58d87780501c7a83204bda8862c72ebb064a6318
Ксения <ksenia2833@gmail.com>
2022-09-13 12:53:44 +0400

    Update LoginForm.js

--- public/js/ui/forms/LoginForm.js
+++ public/js/ui/forms/LoginForm.js
@@ -2,7 +2,7 @@
  * Класс LoginForm управляет формой
  * входа в портал
  * */
-class LoginForm extends AsyncForm {
+ class LoginForm extends AsyncForm {
   /**
    * Производит авторизацию с помощью User.login
    * После успешной авторизации, сбрасывает форму,
@@ -10,6 +10,18 @@ class LoginForm extends AsyncForm {
    * закрывает окно, в котором находится форма
    * */
   onSubmit(data) {
-
+  	User.login(data, callback);
+  	const formControl = Array.prototype.slice.call(document.querySelectorAll('form[id="login-form"] > .form-group > .form-control'));
+  	const modalLogin = document.getElementById('modal-login');
+  	function callback(err, response) {
+  		if (response.success) {
+  			for (let i = 0; i < formControl.length; i++) {
+  				formControl[i].value = '';
+  			}
+  			App.setState('user-logged');
+  			const modalForm = new Modal(modalLogin);
+  			modalForm.close();
+  		}
+  	}
   }
 }
